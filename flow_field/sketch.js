482c51c6f029085193ea34e745295a7e625db588
yashikaadesai <desaiya2@msu.edu>
2025-01-07 16:45:20 -0500

    added zoff for motion

--- flow_field/sketch.js
+++ flow_field/sketch.js
@@ -1,6 +1,7 @@
 let cols, rows;
 let scale = 20;
-let inc = 0.1;
+let inc = 0.1; 
+let zoff = 0; // Third dimension for motion
 let flowfield = [];
 let particles = [];
 
@@ -10,11 +11,12 @@ function setup() {
   rows = floor(height / scale);
 
   flowfield = new Array(cols * rows);
-  for (let i = 0; i < 500; i++) {
+
+  for (let i = 0; i < 200; i++) {
     particles.push(new Particle());
   }
 
-  background(0);
+  background(0); // change background if needed
 }
 
 function draw() {
@@ -23,13 +25,14 @@ function draw() {
     let xoff = 0;
     for (let x = 0; x < cols; x++) {
       let index = x + y * cols;
-      let angle = noise(xoff, yoff) * TWO_PI * 4;
+      let angle = noise(xoff, yoff, zoff) * TWO_PI * 4; // Using zoff for motion
       let v = p5.Vector.fromAngle(angle);
       flowfield[index] = v;
       xoff += inc;
     }
     yoff += inc;
   }
+  zoff += 0.001; // Slowly increment zoff for smooth motion
 
   for (let particle of particles) {
     particle.follow(flowfield);
@@ -44,7 +47,8 @@ class Particle {
     this.pos = createVector(random(width), random(height));
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
-    this.maxSpeed = 4;
+    this.maxSpeed = 2.5;   // try changing the speed of the motion here 
+    this.prevPos = this.pos.copy();
   }
 
   follow(vectors) {
@@ -67,15 +71,28 @@ class Particle {
   }
 
   edges() {
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.prevPos.x = this.pos.x;
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.prevPos.x = this.pos.x;
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.prevPos.y = this.pos.y;
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.prevPos.y = this.pos.y;
+    }
   }
 
   show() {
     stroke(138, 43, 226, 50); // Purple with transparency
     strokeWeight(1);
-    point(this.pos.x, this.pos.y);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y); // Draw lines
+    this.prevPos = this.pos.copy(); // Update the previous position
   }
 }
