fe529c2980b52156d14a03bfbf7c2d4cfa8497b7
yashikaadesai <desaiya2@msu.edu>
2025-01-09 15:44:03 -0500

    implemented basic parts of tulip

--- tulip/sketch.js
+++ tulip/sketch.js
@@ -17,4 +17,29 @@ function setup() {
       drawPetal();
       pop();
     }
+  // Draw stem
+  drawStem();
+}
+
+// Function to draw a single petal
+function drawPetal() {
+  let gradientSteps = 100; // Steps for gradient
+  let petalWidth = 150;
+  let petalHeight = 300;
+
+  for (let i = 0; i < gradientSteps; i++) {
+    let t = i / gradientSteps;
+    let hue = map(t, 0, 1, 180, 300); // Iridescent range
+    let alpha = map(t, 0, 1, 100, 0); // Fade out
+    stroke(hue, 80, 100, alpha);
+    strokeWeight(2);
+
+    // Control points for Bezier curves
+    bezier(
+      0, 0,
+      -petalWidth * (1 - t), -petalHeight * t,
+      petalWidth * (1 - t), -petalHeight * t,
+      0, -petalHeight
+    );
+  }
 }
